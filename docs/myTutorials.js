e537ddc886934503b5e46f88e8334d89fb8e7a4c
a-carson <60159282+a-carson@users.noreply.github.com>
2020-04-10 22:01:45 +0100

    igb

---
+++ docs/myTutorials.js
@@ -0,0 +1,24 @@
+var array =
+[
+{
+"name": "Ali",
+"category": "fsman",
+"memory": "balasdfkhabs"
+},
+{
+"name": "Natasha",
+"category": "bleh",
+"memory": "mckeeeeen"
+}
+]
+
+addToArray({ name: "Ali", category: "Travel", memory: "prikcs" });
+
+
+
+function getArray()
+{
+  return array;
+}
+
+myFunction(array);
