1383b7330a24c2e2a91cc8ca796b351175e739f8
a-carson <60159282+a-carson@users.noreply.github.com>
2020-04-16 13:16:21 +0100

    changes

--- php_testing.php
+++ php_testing.php
@@ -2,9 +2,19 @@
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?>
+  <?php
+    // JSON string
+    $some_json = file_get_contents('json/memories.json');
+    // Convert JSON string to Array
+    $someArray = json_decode($some_json, true);
+    print_r($someArray);        // Dump all data of the Array
+    echo $someArray[0]["name"]; // Access Array data
+
+    // Convert JSON string to Object
+    //$someObject = json_decode($someJSON);
+    //print_r($someObject);      // Dump all data of the Object
+    //echo $someObject[0]->name; // Access Object data
+  ?>
 
 </body>
 </html>
