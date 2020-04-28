76344dbea5544c181d8bec55d21f46c377cd38ba
a-carson <60159282+a-carson@users.noreply.github.com>
2020-04-28 17:35:11 +0100

    Update your_memory.php

--- ui/your_memory.php
+++ ui/your_memory.php
@@ -83,6 +83,7 @@
 		$_SESSION["s"] = $s;
 		$_SESSION["b"] = $b;
 
+
     function test_for_hackers($data)
     {
       $data = trim($data);
@@ -96,6 +97,16 @@
     ?>
 
 		<body id="body3">
+			<div class="backicon-fixed">
+				<a href="record_memory.html">
+					<svg t="1588027382315" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+						p-id="1479" width="48" height="48">
+						<path
+							d="M960.243 456.737v110.526h-682.875l312.547 315.789-78.793 78.948-449.121-450 449.122-450 81.419 78.948-315.173 315.789h682.875z"
+							p-id="1480" fill="var(--ccyan)"></path>
+					</svg>
+				</a>
+			</div>
 
 				<div style="text-align: center;">
 					<h1 class="memory-title" style="margin: 40px">Your Memory</h1>
