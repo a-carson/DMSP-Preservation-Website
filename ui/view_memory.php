c7210d9a75e54dfba8a486450a18086cde87d9f7
a-carson <60159282+a-carson@users.noreply.github.com>
2020-05-04 22:00:05 +0100

    noise working but levels funny

--- ui/view_memory.php
+++ ui/view_memory.php
@@ -28,7 +28,7 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
 <script src="https://unpkg.com/tone"></script>
-<script type="text/javascript" src="../js/sound_and_vision.js"></script>
+<!--<script type="text/javascript" src="../js/sound_and_vision.js"></script>-->
 
 <link href="https://fonts.googleapis.com/css2?family=Orbitron" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400&display=swap" rel="stylesheet">
@@ -102,6 +102,215 @@ $b = $memoriesArray[$index]["b"];
 </body>
 
 <script>
+// JAVA SCRIPT CODE FOR SONIFICATION AND VISUALIZATION
+//
+// DMSP PRESERVATION GROUP
+
+
+// VARIABLES -------------------------------------------------------------------
+var memory = '';
+var string = '';
+var typeOutput = true;
+var textLength;
+var midi = [0, 0];
+var freqs = [0, 0];
+var octave = 0;
+var i = 0;
+var mute = true;
+var t = 0;
+var dur = 15;
+var delta;
+var synthA;
+var synthB;
+const blockSize = 1024;
+var master = Tone.Master;
+let waveform = new Tone.Waveform(blockSize);
+
+// EFFECTS
+var synthVol = new Tone.Volume();
+synthVol.volume.value = -30;
+
+var phaser = new Tone.Phaser({
+  "frequency": 0.5
+});
+var delay = new Tone.FeedbackDelay(0.5);
+delay.wet.value = 0.1;
+var lpf = new Tone.Filter(4000, "lowpass", -12);
+lpf.Q.value = 5;
+var childhood = false;
+function setUpSynth(category)
+{
+switch (category)
+{
+  case "childhood":
+      synthA = new Tone.Synth();
+      synthA.oscillator.type = 'triangle';
+      synthA.chain(waveform, delay, synthVol, master);
+      synthB = new Tone.Synth();
+      synthB.oscillator.type = 'triangle';
+      synthB.chain(waveform, delay, synthVol, master);
+      childhood = true;
+      break;
+
+  case "travel":
+      synthA = new Tone.DuoSynth();
+      synthA.voice1.oscillator.type = "sawtooth"
+      synthA.harmonicity.value = 1;
+      synthA.chain(phaser, waveform, synthVol, master);
+      break;
+  case "student-life":
+      synthA = new Tone.FMSynth();
+      synthA.oscillator.type = "square";
+      synthA.harmonicity.value = 1;
+      synthA.chain(waveform, synthVol, master);
+      synthA.portamento = 0.03;
+      break;
+
+  case "others":
+      synthA = new Tone.DuoSynth();
+      synthA.harmonicity.value = 0.75;
+      synthA.chain(waveform, synthVol, master);
+      break;
+}
+
+}
+
+Tone.Transport.loopEnd = dur;
+Tone.Transport.loop = true;
+
+
+// TEXT TO CODE -------------------------- -------------------------------------
+function setInputText(string) {
+  memory = string;
+  textLength = memory.length;
+  dur = textLength / 2;
+  for (var j = 0; j < textLength; j++) {
+    midi[j] = memory.charCodeAt(j) + octave;
+    freqs[j] = 440 * Math.pow(2, (midi[j] - 69) / 12);
+  }
+}
+
+// SOUND ----------------------------------------------------------------------
+function play() {
+  Tone.Transport.toggle();
+  Tone.Transport.loopEnd = dur;
+  i = 0;
+  t = 0;
+  mute = !mute;
+  Tone.Master.mute = mute;
+}
+
+function sound() {
+  delta = (dur / textLength) * 0.5;
+
+  // Plays next note of array
+  function triggerSynth(time) {
+    i %= textLength;
+    synthA.triggerAttackRelease(freqs[i], '8n', time);
+    if (childhood)
+        synthB.triggerAttackRelease(0.99 * freqs[i], '8n', time);
+    // output text characters
+
+
+    var letter = String.fromCharCode(midi[i]);
+    i++;
+
+    if (typeOutput) {
+      string = string.concat(letter);
+    }
+
+    if (string.length == textLength - 1)
+    {
+      typeOutput = false;
+      Tone.Transport.stop();
+      ready();
+    }
+
+    console.log(string);
+    document.getElementById("letters").innerHTML = string;
+  }
+
+  // Schedules and calls the notes
+  for (var i = 0; i < textLength; i++) {
+    Tone.Transport.schedule(triggerSynth, t);
+    if (midi[i] - octave == 32) {
+      t += delta * 3;
+    } else if (midi[i] - octave == 44) {
+      t += delta * 3;
+    } else if (midi[i] - octave == 46) {
+      t += delta * 6;
+    } else t += delta;
+  }
+
+}
+
+// VISUALS --------------------------------------------------------------------
+let numCircles = 8;
+let spacing = 1;
+var x1 = new Uint32Array(numCircles);
+var x2 = new Uint32Array(numCircles);
+var y1 = new Uint32Array(numCircles);
+var y2 = new Uint32Array(numCircles);
+var strokes = new Uint32Array(numCircles);
+var h = new Uint32Array(numCircles);
+var s = new Uint32Array(numCircles);
+var b = new Uint32Array(numCircles);
+
+
+function setup() {
+  let cnv = createCanvas(600, 600);
+  cnv.parent('sketch-holder');
+  noSmooth();
+  colorMode(HSB, 360);
+  cnv.mouseOver(() => noiseOn = true);
+  cnv.mouseOut(() => noiseOn = false);
+  noiseSynth.triggerAttack();
+
+  //console.log("setup success");
+}
+
+function draw() {
+  background(frameCount % 360, 360, 43);
+  push();
+  pop();
+  sound();
+  drawWaveform();
+  noiseFcn();
+}
+
+
+function drawWaveform() {
+  let linesX = 40;
+  let linesY = 26;
+  let stepsX = width / 40;
+  let stepsY = height / 26;
+  let a = 0;
+  let angle = (2 * PI) / 100; //can change these to change the shape originl /100
+  let step = 3; // can change these to change the shape
+  //let step = floor(waveform.size / 300);
+
+
+  for (let i = 0; i < waveform.size - step; i += step) {
+    let value = waveform.getValue()[i];
+    let stepValue = waveform.getValue()[i + step];
+
+    for (let j = 0; j < numCircles; j++) {
+      let denom = 2 * Math.pow((1 + j * spacing), 0.8);
+      x1[j] = width / 2 + cos(a) * (width / 2 * (value + 1) / denom);
+      y1[j] = height / 2 + sin(a) * (width / 2 * (value + 1) / denom);
+      x2[j] = width / 2 + cos(a + angle) * (width / 2 * (stepValue + 1) / denom);
+      y2[j] = height / 2 + sin(a + angle) * (width / 2 * (stepValue + 1) / denom);
+      stroke(h[j], s[j], b[j]);
+      strokeWeight(strokes[j]);
+      line(x1[j], y1[j], x2[j], y2[j]);
+    }
+
+    a += angle;
+  }
+
+}
+
+
   var memory_data = '<?php echo json_encode($memory); ?>';
   var text = JSON.parse(memory_data);
   setUpSynth(JSON.parse('<?php echo json_encode($category); ?>'));
@@ -123,20 +332,67 @@ $b = $memoriesArray[$index]["b"];
 
   "use strict";
 
+  // NOISE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  var noiseSynth = new Tone.NoiseSynth({
+  "envelope" : {
+  "attack" : 0.5 ,
+  "decay" : 0.1 ,
+  "sustain" : 1
+  }
+  });
+
+  var filterSweep = new Tone.AutoFilter();
+  filterSweep.baseFrequency.value = 100;
+  filterSweep.octaves.value = 6;
+  filterSweep.frequency.value = 0.1;
+
+  var noiseVol = new Tone.Volume(-40);
+  noiseSynth.chain(filterSweep, noiseVol, waveform, master);
+  noiseSynth.sync();
+  var noiseOn = false;
+
+  function noiseFcn()
+  {
+
+    if (noiseOn)
+    {
+          var val = noiseVol.volume.value;
+          if (val < 50)
+          {
+          noiseVol.volume.value += 0.4;
+          synthVol.volume.value -= 0.1;
+          }
+    }
+
+  }
+
+  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
   //ONLOAD
   $(function () {
     //DECLARE GLOBAL VARIABLE FOR USE IN HANDLERS
     var orig, sib;
     var runs = 0;
-    var charSet = '1234567890+>?-$#@%&*'; //RUN JS WHEN 'DISTORT' IS HOVERED
+    var charSet = '   abcdefghijklmnopqrstuvwxyz1234567890+>?-$#@%&*'; //RUN JS WHEN 'DISTORT' IS HOVERED
+    var memory_data = '<?php echo json_encode($memory); ?>';
+    var text = JSON.parse(memory_data);
 
-    $('.distort').hover(function () {
+    $('.distort').hover(function ()
+    {
       var curr = $(this);
       orig = $(this).text();
-      sib = setInterval(function () {
+      noiseFcn();
+
+      sib = setInterval(function ()
+      {
         distortText(curr);
-      }, 500);
-    });
+      }, 200);
+    }, function()
+    {
+      clearInterval(sib);
+      noiseOn = false;
+    }
+  );
 
     function distortText(i) {
       //MAINTAINS SOME READABILITY IN THE TEXT BY ONLY ALLOWING 3 CHARACTERS TO BE DISTORTED
@@ -146,23 +402,28 @@ $b = $memoriesArray[$index]["b"];
       //   return;
       // }
       //GET EACH INDIVIDUAL CHARACTER
+      noiseOn = true;
+
       var chars = i.text().split(''); //GET A RANDOM CHARACTER FROM THE TEXT
 
       var rand = Math.floor(Math.random() * chars.length); //GET A RANDOM REPLACEMENT CHARACTER
 
       var randRep = Math.floor(Math.random() * charSet.length); //CHECK TO MAKE SURE THAT THE NEW CHARACTER IS DIFFERENT FROM THE OLD
 
-      if (chars[rand] != charSet[randRep] && chars[rand] != ' ') {
+      if (chars[rand] != charSet[randRep])
+      {
         chars[rand] = charSet[randRep];
-      } else {
-        distortText(i);
+      }
+      else {
+        //distortText(i);
       } //UPDATE TEXT
 
-
       i.text(chars.join(''));
-      runs += 1;
+      //runs += 1;
     }
   });
+
+
 
 
   </script>
--- ui/your_memory.php
+++ ui/your_memory.php
@@ -25,7 +25,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.1/addons/p5.sound.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://unpkg.com/tone"></script>
-<script type = "text/javascript" src = "../js/sound_and_vision.js"></script>
+<!--<script type = "text/javascript" src = "../js/sound_and_vision.js"></script>-->
 
 <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400&display=swap" rel="stylesheet">
@@ -197,6 +197,251 @@
 		</body>
 
 <script>
+// JAVA SCRIPT CODE FOR SONIFICATION AND VISUALIZATION
+//
+// DMSP PRESERVATION GROUP
+
+
+// VARIABLES -------------------------------------------------------------------
+var memory = '';
+var string = '';
+var typeOutput = true;
+var textLength;
+var midi = [0, 0];
+var freqs = [0, 0];
+var octave = 0;
+var i = 0;
+var mute = true;
+var t = 0;
+var dur = 15;
+var delta;
+var synthA;
+var synthB;
+const blockSize = 1024;
+var master = Tone.Master;
+let waveform = new Tone.Waveform(blockSize);
+//let delay = new Tone.FeedbackDelay(Tone.Time('8n'));
+
+var synthVol = new Tone.Volume();
+synthVol.volume.value = -30;
+console.log(synthVol.volume.value);
+let phaser = new Tone.Phaser({
+  "frequency": 0.5
+});
+
+var delay = new Tone.FeedbackDelay(0.5);
+delay.wet.value = 0.1;
+var lpf = new Tone.Filter(4000, "lowpass", -12);
+lpf.Q.value = 5;
+var childhood = false;
+
+function setUpSynth(category)
+{
+switch (category)
+{
+  case "childhood":
+      synthA = new Tone.Synth();
+      synthA.oscillator.type = 'triangle';
+      synthA.chain(waveform, delay, synthVol, master);
+      synthB = new Tone.Synth();
+      synthB.oscillator.type = 'triangle';
+      synthB.chain(waveform, delay, synthVol, master);
+      childhood = true;
+      break;
+
+  case "travel":
+      synthA = new Tone.DuoSynth();
+      synthA.voice1.oscillator.type = "sawtooth"
+      synthA.harmonicity.value = 1;
+      synthA.chain(phaser, waveform, synthVol, master);
+      break;
+  case "student-life":
+      synthA = new Tone.FMSynth();
+      synthA.oscillator.type = "square";
+      synthA.harmonicity.value = 1;
+      synthA.chain(waveform, synthVol, master);
+      synthA.portamento = 0.03;
+      break;
+
+  case "others":
+      synthA = new Tone.DuoSynth();
+      synthA.harmonicity.value = 0.75;
+      synthA.chain(waveform, synthVol, master);
+      break;
+}
+
+}
+
+Tone.Transport.loopEnd = dur;
+Tone.Transport.loop = true;
+
+
+// TEXT TO CODE -------------------------- -------------------------------------
+function setInputText(string) {
+  memory = string;
+  textLength = memory.length;
+  dur = textLength / 2;
+  for (var j = 0; j < textLength; j++) {
+    midi[j] = memory.charCodeAt(j) + octave;
+    freqs[j] = 440 * Math.pow(2, (midi[j] - 69) / 12);
+  }
+}
+
+// SOUND ----------------------------------------------------------------------
+function play() {
+  Tone.Transport.toggle();
+  Tone.Transport.loopEnd = dur;
+  i = 0;
+  t = 0;
+  mute = !mute;
+  Tone.Master.mute = mute;
+}
+
+function sound() {
+  delta = (dur / textLength) * 0.5;
+
+  // Plays next note of array
+  function triggerSynth(time) {
+    i %= textLength;
+    synthA.triggerAttackRelease(freqs[i], '8n', time);
+    if (childhood)
+        synthB.triggerAttackRelease(0.99 * freqs[i], '8n', time);
+    // output text characters
+    var letter = String.fromCharCode(midi[i]);
+    i++;
+
+    if (typeOutput) {
+      string = string.concat(letter);
+    }
+
+    if (string.length == textLength - 1)
+    {
+      typeOutput = false;
+      Tone.Transport.stop();
+      ready();
+    }
+
+    console.log(string);
+    document.getElementById("letters").innerHTML = string;
+  }
+
+  // Schedules and calls the notes
+  for (var i = 0; i < textLength; i++) {
+    Tone.Transport.schedule(triggerSynth, t);
+    if (midi[i] - octave == 32) {
+      t += delta * 3;
+    } else if (midi[i] - octave == 44) {
+      t += delta * 3;
+    } else if (midi[i] - octave == 46) {
+      t += delta * 6;
+    } else t += delta;
+  }
+
+}
+
+// VISUALS --------------------------------------------------------------------
+let numCircles = 8;
+let spacing = 1;
+var x1 = new Uint32Array(numCircles);
+var x2 = new Uint32Array(numCircles);
+var y1 = new Uint32Array(numCircles);
+var y2 = new Uint32Array(numCircles);
+var strokes = new Uint32Array(numCircles);
+var h = new Uint32Array(numCircles);
+var s = new Uint32Array(numCircles);
+var b = new Uint32Array(numCircles);
+
+
+function setup() {
+  let cnv = createCanvas(600, 600);
+  cnv.parent('sketch-holder');
+  noSmooth();
+  colorMode(HSB, 360);
+  //console.log("setup success");
+}
+
+function draw() {
+  background(frameCount % 360, 360, 43);
+  push();
+  pop();
+  sound();
+  drawWaveform();
+}
+
+
+function drawWaveform() {
+  let linesX = 40;
+  let linesY = 26;
+  let stepsX = width / 40;
+  let stepsY = height / 26;
+  let a = 0;
+  let angle = (2 * PI) / 100; //can change these to change the shape originl /100
+  let step = 3; // can change these to change the shape
+  //let step = floor(waveform.size / 300);
+
+
+  for (let i = 0; i < waveform.size - step; i += step) {
+    let value = waveform.getValue()[i];
+    let stepValue = waveform.getValue()[i + step];
+
+    for (let j = 0; j < numCircles; j++) {
+      let denom = 2 * Math.pow((1 + j * spacing), 0.8);
+      x1[j] = width / 2 + cos(a) * (width / 2 * (value + 1) / denom);
+      y1[j] = height / 2 + sin(a) * (width / 2 * (value + 1) / denom);
+      x2[j] = width / 2 + cos(a + angle) * (width / 2 * (stepValue + 1) / denom);
+      y2[j] = height / 2 + sin(a + angle) * (width / 2 * (stepValue + 1) / denom);
+      stroke(h[j], s[j], b[j]);
+      strokeWeight(strokes[j]);
+      line(x1[j], y1[j], x2[j], y2[j]);
+    }
+
+    a += angle;
+  }
+
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+// Generates semi-random colours
+function setRandomColoursByCategory(colour) {
+  var offset;
+  var range = 90;
+
+  if (colour.localeCompare('travel') == 0) {
+    // yellow
+    offset = 0;
+    range = 90;
+  }
+
+  if (colour.localeCompare('others') == 0) {
+    // green
+    offset = 90;
+    range = 90;
+  }
+
+  if (colour.localeCompare('student-life') == 0) {
+    // blue
+    offset = 180;
+    range = 90;
+  }
+
+  if (colour.localeCompare('childhood') == 0) {
+    // pink
+    offset = 270;
+    range = 90;
+  }
+
+  for (let j = 0; j < numCircles; j++) {
+    strokes[j] = 3 - 0.3 * j;
+    h[j] = offset + getRndInteger(0, range);
+    s[j] = getRndInteger(70, 360);
+    b[j] = getRndInteger(210, 360);
+  }
+}
+
 var memory_data = '<?php echo json_encode($memory); ?>'
 var text = JSON.parse(memory_data);
 setUpSynth(JSON.parse('<?php echo json_encode($category); ?>'));
